51cb47d278bf5e221c2ba9afe191747fe4cf92ab
IThundxr <harshdhaliwal9767@gmail.com>
2022-07-29 22:25:34 -0400

    Late night fixes

--- src/main/java/dev/ithundxr/mods/farwateraddons/util/ModTags.java
+++ src/main/java/dev/ithundxr/mods/farwateraddons/util/ModTags.java
@@ -4,6 +4,7 @@ import dev.ithundxr.mods.farwateraddons.FarwaterAddons;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.Tags;
@@ -12,24 +13,24 @@ public class ModTags {
     public static class Blocks {
         //stuff here
 
-        private static Tags.IOptionalNamedTag<Block> tag(String name) {
-            return BlockTags.createOptional(new ResourceLocation(FarwaterAddons.MOD_ID, name));
+        private static TagKey<Block> tag(String name) {
+            return BlockTags.create(new ResourceLocation(FarwaterAddons.MOD_ID, name));
         }
 
-        private static Tags.IOptionalNamedTag<Block> forgeTag(String name) {
-            return BlockTags.createOptional(new ResourceLocation("forge", name));
+        private static TagKey<Block> forgeTag(String name) {
+            return BlockTags.create(new ResourceLocation("forge", name));
         }
     }
 
     public static class Items {
         //stuff here
 
-        private static Tags.IOptionalNamedTag<Item> tag(String name) {
-            return ItemTags.createOptional(new ResourceLocation(FarwaterAddons.MOD_ID, name));
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(new ResourceLocation(FarwaterAddons.MOD_ID, name));
         }
 
-        private static Tags.IOptionalNamedTag<Item> forgeTag(String name) {
-            return ItemTags.createOptional(new ResourceLocation("forge", name));
+        private static TagKey<Item> forgeTag(String name) {
+            return ItemTags.create(new ResourceLocation("forge", name));
         }
     }
 }
